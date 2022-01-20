eba03ceef777aa31112f15e1ca62f413a14de294
Abderrahim Bahia <abderrahimbahia19@gmail.com>
2022-01-19 20:50:41 -0500

    completed StockId file

--- LabTwo/Program.cs
+++ LabTwo/Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿//Programmer: Abderrahim Bahia
+//Overview: Driver program applies StockItem program to a Milk and Bread Store
+//Last Update: 01/19/2022
 
+using System;
 namespace LabTwo
 {
-    class Program
+    class Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            StockItem milk = new StockItem("1 Gallon of Milk", 3.60f, 15);
+            StockItem bread = new StockItem("1 Loaf of Bread", 1.98f, 30);
+
+            int choice = 8;
+            do
+            {
+                Console.WriteLine("1. Sold One Milk");
+                Console.WriteLine("2.Sold One Bread");
+                Console.WriteLine("3.Change price of Milk");
+                Console.WriteLine("4.Change price of Bread");
+                Console.WriteLine("5.Add Milk to Inventory");
+                Console.WriteLine("6.Add Bread to Inventory");
+                Console.WriteLine("7.See Inventory");
+                Console.WriteLine("8.Quit");
+                choice = Int32.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    milk.lowerQuantity(1);
+                }
+                else if (choice == 2)
+                {
+                    bread.lowerQuantity(1);
+                }
+                else if (choice == 3)
+                {
+                    Console.Write("Please enter a new price");
+                    float price = float.Parse(Console.ReadLine());
+
+                    milk.setPrice(price);
+                }
+                else if (choice == 4)
+                {
+                    Console.Write("Please enter a new price");
+                    float price = float.Parse(Console.ReadLine());
+
+                    bread.setPrice(price);
+                }
+                else if (choice == 5)
+                {
+                    Console.Write("Please enter the quantity to add");
+                    int quantirty = Int32.Parse(Console.ReadLine());
+
+                    milk.raiseQuantity(quantirty);
+                }
+                else if (choice == 6)
+                {
+                    Console.Write("Please enter the quantity to add");
+                    int quantirty = Int32.Parse(Console.ReadLine());
+
+                    bread.raiseQuantity(quantirty);
+                }
+                else if (choice == 7)
+                {
+                    milk.ToString();
+                    bread.ToString();
+                }
+
+            } while (choice != 8);
+
         }
     }
 }
---
+++ LabTwo/StockItem.cs
@@ -0,0 +1,96 @@
+﻿//Programmer: Abderrahim Bahia
+//Overview: StockItem is a class used to track items in a stock at a store
+//Last Update: 01/19/2022
+
+using System;
+namespace LabTwo
+{
+    public class StockItem
+    {
+
+        private string description;
+        public static int id;
+        private float price;
+        private int quantity;
+
+
+        public StockItem()
+        {
+            description = "No description provided";
+            id = 1;
+            id++;
+            price = 0;
+            quantity = 1;
+        }
+
+
+        public StockItem(string description, float price, int quantity)
+        {
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+            id = 1;
+            id++;
+
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+
+        public string getDescription()
+        {
+            return description;
+        }
+
+
+        public float getPrice()
+        {
+            return price;
+        }
+
+
+        public void setPrice(float price) {
+
+            if (price < 0)
+            {
+                Console.Write("Please enter a valid price");
+            }
+            else
+            {
+                this.price = price;
+            }
+        }
+
+
+        public int getQuantity()
+        {
+            return this.quantity;
+        }
+
+
+        public void lowerQuantity(int quantity)
+        {
+            this.quantity -= quantity;
+        }
+
+
+        public void raiseQuantity(int quantity)
+        {
+            this.quantity += quantity;
+        }
+
+
+        public override string ToString()
+        {
+            return "Item Description: " + description +
+                   "Item ID: " + id +
+                   "Item Quantity: " + quantity +
+                   "Item Price: " + price;
+        }
+
+
+    }
+}
